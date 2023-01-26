14a5ebcf6e5b20f92eb304d6212b1213c09be801
Vansh Tanwani <vanshtanwani.vt@gmail.com>
2023-01-26 18:37:36 +0530

    Day-16

---
+++ Day-16/assets/bounce.js
@@ -0,0 +1,25 @@
+var score = 0;
+var ball;
+var platforms;
+var cursors;
+var star;
+var scoreText;
+var goal;
+var starCount;
+var nail;
+var pit;
+
+var config={
+    type:Phaser.AUTO,
+    width: 1400,
+    height: 800,
+    physics: {
+        default: "arcade",
+        arcade: {
+            gravity: { y: 300 },
+            debug: false
+        }
+    },
+    scene:[LVL8,LVLComplete,GameOver],
+};
+var game = new Phaser.Game(config);
